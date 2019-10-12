6bb47de8fe4849c09d0faaed4c4141ff5608c1ca
victorungu99 <victorungu99@gmail.com>
2019-10-12 20:49:24 +0300

    Rewrote SetupReader

--- include/gmolmodel/SetupReader.hpp
+++ include/gmolmodel/SetupReader.hpp
@@ -1,38 +1,41 @@
-#ifndef __SETUPREADER_HPP__
-#define __SETUPREADER_HPP__
+#pragma once
 
-#include "Robo.hpp"
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <string>
+#include <vector>
 
 // This class implements the functionality for reading arguments from a setup
 // file and give back the necessary values as vectors
-class SetupReader{
+class SetupReader {
 public:
 
-    // Constructor
-    SetupReader(const char *FN);
-    SetupReader(std::string& FN);
+	// Constructor
+	SetupReader(const char *FN);
+	SetupReader(const std::string& FN);
 
-    // Destructor
-    ~SetupReader();
+	// Read setup function
+	void ReadSetup(const char *FN);
+	void ReadSetup(const std::string& FN);
 
-    // Read setup function
-    void ReadSetup(const char *FN);
-    void ReadSetup(std::string& FN);
+	// Print all the arguments
+	void dump() const;
 
-    // Print all the arguments
-    void dump(void);
+	// Access values by key
+	bool find(const char *argKey) const;
+	bool find(const std::string& argKey) const;
 
-    // Access values by key
-    bool find( std::string argKey );
-
-    std::vector<std::string> get(const char *argKey);
-    std::vector<std::string> get(std::string argKey);
+	const std::vector<std::string>& get(const char *argKey) const;
+	const std::vector<std::string>& get(const std::string& argKey) const;
 
 private:
-    std::map<std::string, std::vector<std::string>> Args; // arguments
-    std::map<std::string, std::vector<std::string>>::iterator ArgsIt; // arguments
+	std::map<std::string, std::vector<std::string>> Args; // arguments
+
+	bool IsPrintable(const std::string& s) const;
+
+	std::vector<std::string> KeyNotFound = { "ERROR_KEY_NOT_FOUND" };
 };
-
-
-#endif //__SETUPREADER_HPP__
 
