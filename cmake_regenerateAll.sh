9a88bbea257e1fdfc1c0d25cd04048a895329546
Laurentiu Spiridon <spiridon.laurentiu@gmail.com>
2019-11-14 14:53:14 +0200

    cmake_regenerateAll

--- cmake_regenerateAll.sh
+++ cmake_regenerateAll.sh
@@ -4,16 +4,16 @@ cd ../Molmodel/Simbody01
 mkdir build-debug
 cd build-debug
 cmake -DCMAKE_BUILD_TYPE=Debug  ..
-make -j4
+make -j16
 sudo make install
 
 cd ../../
 mkdir build-debug
 cd build-debug
 cmake -DCMAKE_BUILD_TYPE=Debug ..
-make -j4
+make -j16
 sudo make install
 
 cd ../../build-debug/
 cmake ..
-make -j4
+make -j16
