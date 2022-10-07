df60d062c79d016beda843cf7d11b2158fa4a32d
Laurentiu Spiridon <spiridon.laurentiu@gmail.com>
2022-10-07 12:53:42 +0300

    OS memory 1

--- src/Robosample.cpp
+++ src/Robosample.cpp
@@ -111,6 +111,7 @@ std::string exec(const char* cmd) {
     std::string result;
     std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
     if (!pipe) {
+	printf ("No pipe with error: %s\n",strerror(errno));
         throw std::runtime_error("popen() failed!");
     }
     while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
