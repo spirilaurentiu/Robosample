7c23417fa8a88cb5b3fd33ca085ce62e07abe82b
Laurentiu Spiridon <spiridon.laurentiu@gmail.com>
2023-03-07 08:16:11 +0200

    stretch symmetric diffs test 3a

--- src/Context.cpp
+++ src/Context.cpp
@@ -3583,7 +3583,7 @@ void Context::RunRENS(void)
 	PrintNofAcceptedSwapsMatrix();
 
 		// DELETE THIS CODE
-		std::cout << "TEST MODE\n";
+		/* std::cout << "TEST MODE\n";
 		std::vector<SimTK::Real> givenX_PF(22, 999);
 		std::vector<SimTK::Real> givenX_BM(22, 999);
 
@@ -3632,7 +3632,7 @@ void Context::RunRENS(void)
 		givenX_BM[20] = 0.108296974;
 		givenX_BM[21] = 0.111305194;
 
-		worlds[0].setTransformsMeans(givenX_PF, givenX_BM);
+		worlds[0].setTransformsMeans(givenX_PF, givenX_BM); */
 		// DELETE CODE ABOVE
 
 	// Main loop
