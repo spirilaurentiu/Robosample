40888a01dfa4ea5ae089b98ca797f96304633ad2
Laurentiu Spiridon <spiridon.laurentiu@gmail.com>
2023-03-28 16:31:16 +0300

    Change std of bendstretch

--- src/Context.cpp
+++ src/Context.cpp
@@ -2652,7 +2652,7 @@ bool Context::attemptRENSSwap(int replica_i, int replica_j)
 		<< std::endl;
 
 
-	SimTK::Real log_p_accept = WTerm ; //+ std::log(correctionTerm);
+	SimTK::Real log_p_accept = WTerm + std::log(correctionTerm);
 
 	// Draw from uniform distribution
 	SimTK::Real unifSample = uniformRealDistribution(randomEngine);
@@ -3617,7 +3617,7 @@ void Context::RunRENS(void)
 	PrintNofAcceptedSwapsMatrix();
 
 		// DELETE THIS CODE
-		std::cout << "TEST MODE\n";
+		/*std::cout << "TEST MODE\n";
 		std::vector<SimTK::Real> givenX_PF(22, 999);
 		std::vector<SimTK::Real> givenX_BM(22, 999);
 
@@ -3666,7 +3666,7 @@ void Context::RunRENS(void)
 		givenX_BM[20] = 0.108296974;
 		givenX_BM[21] = 0.111305194;
 
-		worlds[0].setTransformsMeans(givenX_PF, givenX_BM);
+		worlds[0].setTransformsMeans(givenX_PF, givenX_BM);*/
 		// DELETE CODE ABOVE
 
 	// Main loop
@@ -3684,13 +3684,13 @@ void Context::RunRENS(void)
 		}
 
 		// Apply other functions to scale factors
-		for(auto& qsf : qScaleFactorsMiu){
+		/* for(auto& qsf : qScaleFactorsMiu){
 			if(qsf < 1.0){
 				qsf = 1.0;
 			}else{
 				qsf = 1.0;
 			}
-		}
+		} */
 
 		// Run each replica serially for equilibrium worlds
 		for (size_t replicaIx = 0; replicaIx < nofReplicas; replicaIx++){
