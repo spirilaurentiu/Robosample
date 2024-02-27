914444d99fdf74870cdee7a69699d1e7fc903e2e
victorungu99 <victorungu99@gmail.com>
2024-02-27 16:53:42 +0200

    fixed duplication of variable declaration

--- include/Context.hpp
+++ include/Context.hpp
@@ -664,11 +664,6 @@ private:
 	//std::vector<std::string> rootMobilities;
 	InternalCoordinates internCoords;
 
-	/** Vectors of Cartesian coordinates **/
-	std::vector<SimTK::Real> Xs;
-	std::vector<SimTK::Real> Ys;
-	std::vector<SimTK::Real> Zs;
-
 	// WORLD END
 
 	// Simulation parameters
