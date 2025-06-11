c712d676f18a6b5a389568dec997752bb6519df9
Laurentiu Spiridon <spiridon.laurentiu@gmail.com>
2025-06-11 16:09:37 +0300

    OrthoSpherical ala1

--- src/HMCSampler.cpp
+++ src/HMCSampler.cpp
@@ -1114,9 +1114,9 @@ void HMCSampler::perturbPositions(SimTK::State& someState, PositionsPerturbMetho
 						bool doTorsionStretch = false;
 						bool do_TorsionMapping = false;
 
-						if(numUs > 0){
-							do_SliderStretch = true;
-						}
+						// if(numUs > 0){
+						// 	do_SliderStretch = true;
+						// }
 						if(numUs > 1){
 							do_AngleStretch = true;
 						}
@@ -1125,12 +1125,19 @@ void HMCSampler::perturbPositions(SimTK::State& someState, PositionsPerturbMetho
 
 						if(do_SliderStretch){
 
-							//if(	(int(mbx) == 4) || (int(mbx) == 5) || (int(mbx) == 6) ||  (int(mbx) == 7) || (int(mbx) == 8) || (int(mbx) == 2) 
-							if(true
-							){
+							//if( false
+							//    || (int(mbx) == 4) || (int(mbx) == 5) || (int(mbx) == 6) // ETHANE BAT bonds
+							//	||  (int(mbx) == 7) || (int(mbx) == 8) || (int(mbx) == 2) // ETHANE perpe bonds
+							//){ // ethane
+							if( false
+								|| (int(mbx) == 16) || (int(mbx) == 17) || (int(mbx) == 18)    || (int(mbx) == 12) // ALA1 side methyl
+								|| (int(mbx) == 19) || (int(mbx) == 20) || (int(mbx) == 21)    || (int(mbx) == 14) // ALA1 C-ter methyl
+								|| (int(mbx) == 3) || (int(mbx) == 8)       || (int(mbx) == 4) || (int(mbx) == 10) // ALA1 N-ter peptide bond
+								|| (int(mbx) == 9) || (int(mbx) == 15)       || (int(mbx) == 11) || (int(mbx) == 22) // ALA1 C-ter peptide bond
+							  ){
 								SimTK::Real dBMp_local = BONDLengths[zMatRow] - (*prev_BMps_means)[int(mbx)];
-								std::cout << "check bond " << BONDLengths[zMatRow] <<" "<< X_BM.p().norm() << std::endl;
-								std::cout << "B Bmean dBMp_local" <<" "<< BONDLengths[zMatRow] <<" "<< (*prev_BMps_means)[int(mbx)] <<" "<< dBMp_local << std::endl;
+								std::cout << "BONDLengths X_BM_p_norm " << BONDLengths[zMatRow] <<" "<< X_BM.p().norm() << std::endl;
+								std::cout << "BONDLengths BMps_mean dBMp_local" <<" "<< BONDLengths[zMatRow] <<" "<< (*prev_BMps_means)[int(mbx)] <<" "<< dBMp_local << std::endl;
 
 								if(numUs == 3){
 									if(localQIndex == 2){
@@ -1153,22 +1160,23 @@ void HMCSampler::perturbPositions(SimTK::State& someState, PositionsPerturbMetho
 
 						if(do_AngleStretch){
 
-							// if( (int(mbx) == 4) || (int(mbx) == 5) || (int(mbx) == 6) 
-							// ||  (int(mbx) == 7) || (int(mbx) == 8) // || (int(mbx) == 2)
-							if(true
-							){
-								SimTK::Real dPFr_local = ANGLEBends[zMatRow] - (*prev_PFrs_means)[int(mbx)];
-								std::cout << "check angle " << ANGLEBends[zMatRow] <<" "<< SimTK::Pi - std::acos(X_PF.R()(0)(0)) << std::endl;
-								std::cout << "A Amean dPFr_local" <<" "<< ANGLEBends[zMatRow] <<" "<< SimTK::Pi - (*prev_PFrs_means)[int(mbx)] <<" "<< dPFr_local << std::endl;
+							//if( false
+							//    || (int(mbx) == 4) || (int(mbx) == 5) || (int(mbx) == 6) // ETHANE BAT bonds
+							//	||  (int(mbx) == 7) || (int(mbx) == 8) || (int(mbx) == 2) // ETHANE perpe bonds
+							//){ // ethane
+							if( false 
+								|| (int(mbx) == 16) || (int(mbx) == 17) || (int(mbx) == 18)    //|| (int(mbx) == 12) // ALA1 methyl
+								// || (int(mbx) == 19) || (int(mbx) == 20) || (int(mbx) == 21)    //|| (int(mbx) == 14) // ALA1 methyl
+								// || (int(mbx) == 3) || (int(mbx) == 8)       || (int(mbx) == 4) || (int(mbx) == 10) // ALA1 N-ter peptide bond
+								// || (int(mbx) == 9) || (int(mbx) == 15)       || (int(mbx) == 11) || (int(mbx) == 22) // C-ter peptide bond
+							  ){								
+								SimTK::Real dPFr_local = ANGLEBends[zMatRow] - (SimTK::Pi - (*prev_PFrs_means)[int(mbx)]);
+								std::cout << "ANGLEBends pi_PFrs " << ANGLEBends[zMatRow] <<" "<< SimTK::Pi - std::acos(X_PF.R()(0)(0)) << std::endl;
+								std::cout << "ANGLEBends PFrs_mean pi_PFrs_mean dPFr_local" <<" "<< ANGLEBends[zMatRow] <<" "<< (*prev_PFrs_means)[int(mbx)] <<" "<< SimTK::Pi - (*prev_PFrs_means)[int(mbx)] <<" "<< dPFr_local << std::endl;
 
-								if(numUs == 3){
-									if(localQIndex == 0){
-										stateQs[qIx] += ANGLEBends[zMatRow] * ((scaleFactor) - 1);
-									}
-								}else if(numUs == 2){
-									if(localQIndex == 0){
-										stateQs[qIx] += ANGLEBends[zMatRow] * ((scaleFactor) - 1);
-									}
+								if(false || (localQIndex == 0) // || (localQIndex == 1)
+								){
+									stateQs[qIx] += ANGLEBends[zMatRow] * ((scaleFactor) - 1);
 								}
 
 							} // __end__ which bodies do we stretch
@@ -1230,9 +1238,9 @@ void HMCSampler::perturbPositions(SimTK::State& someState, PositionsPerturbMetho
 						bool doTorsionStretch = false;
 						bool do_TorsionMapping = false;
 
-						//if(numUs > 0){
-						//	do_SliderStretch = true;
-						//}
+						if(numUs > 0){
+							do_SliderStretch = true;
+						}
 						if(numUs > 1){
 							do_AngleStretch = true;
 						}
@@ -1241,11 +1249,19 @@ void HMCSampler::perturbPositions(SimTK::State& someState, PositionsPerturbMetho
 
 						if(do_SliderStretch){
 
-							if(	(int(mbx) == 4) || (int(mbx) == 5) || (int(mbx) == 6) ||  (int(mbx) == 7) || (int(mbx) == 8) || (int(mbx) == 2) 
-							){
+							//if( false
+							//    || (int(mbx) == 4) || (int(mbx) == 5) || (int(mbx) == 6) // ETHANE BAT bonds
+							//	||  (int(mbx) == 7) || (int(mbx) == 8) || (int(mbx) == 2) // ETHANE perpe bonds
+							//){ // ethane
+							if( false
+								|| (int(mbx) == 16) || (int(mbx) == 17) || (int(mbx) == 18)    || (int(mbx) == 12) // ALA1 side methyl
+								|| (int(mbx) == 19) || (int(mbx) == 20) || (int(mbx) == 21)    || (int(mbx) == 14) // ALA1 C-ter methyl
+								|| (int(mbx) == 3) || (int(mbx) == 8)       || (int(mbx) == 4) || (int(mbx) == 10) // ALA1 N-ter peptide bond
+								|| (int(mbx) == 9) || (int(mbx) == 15)       || (int(mbx) == 11) || (int(mbx) == 22) // ALA1 C-ter peptide bond
+							  ){
 								SimTK::Real dBMp_local = BONDLengths[zMatRow] - (*prev_BMps_means)[int(mbx)];
-								// std::cout << "check bond " << BONDLengths[zMatRow] <<" "<< X_BM.p().norm() << std::endl;
-								// std::cout << "B Bmean dBMp_local" <<" "<< BONDLengths[zMatRow] <<" "<< (*prev_BMps_means)[int(mbx)] <<" "<< dBMp_local << std::endl;
+								std::cout << "BONDLengths X_BM_p_norm " << BONDLengths[zMatRow] <<" "<< X_BM.p().norm() << std::endl;
+								std::cout << "BONDLengths BMps_mean dBMp_local" <<" "<< BONDLengths[zMatRow] <<" "<< (*prev_BMps_means)[int(mbx)] <<" "<< dBMp_local << std::endl;
 
 								if(numUs == 3){
 									if(localQIndex == 2){
@@ -1268,14 +1284,19 @@ void HMCSampler::perturbPositions(SimTK::State& someState, PositionsPerturbMetho
 
 						if(do_AngleStretch){
 
-							if( (int(mbx) == 4) 
-							//			|| (int(mbx) == 5) || (int(mbx) == 6) 
-							//||  (int(mbx) == 7)
-							//			|| (int(mbx) == 8) // || (int(mbx) == 2)
-							){
+							//if( false
+							//    || (int(mbx) == 4) || (int(mbx) == 5) || (int(mbx) == 6) // ETHANE BAT bonds
+							//	||  (int(mbx) == 7) || (int(mbx) == 8) || (int(mbx) == 2) // ETHANE perpe bonds
+							//){ // ethane
+							if( false 
+								|| (int(mbx) == 16) || (int(mbx) == 17) || (int(mbx) == 18)    //|| (int(mbx) == 12) // ALA1 methyl
+								|| (int(mbx) == 19) || (int(mbx) == 20) || (int(mbx) == 21)    //|| (int(mbx) == 14) // ALA1 methyl
+								|| (int(mbx) == 3) || (int(mbx) == 8)       || (int(mbx) == 4) || (int(mbx) == 10) // ALA1 N-ter peptide bond
+								|| (int(mbx) == 9) || (int(mbx) == 15)       || (int(mbx) == 11) || (int(mbx) == 22) // C-ter peptide bond
+							  ){
 								SimTK::Real dPFr_local = ANGLEBends[zMatRow] - (SimTK::Pi - (*prev_PFrs_means)[int(mbx)]);
-								// std::cout << "check angle " << ANGLEBends[zMatRow] <<" "<< SimTK::Pi - std::acos(X_PF.R()(0)(0)) << std::endl;
-								// std::cout << "A Amean dPFr_local" <<" "<< ANGLEBends[zMatRow] <<" "<< SimTK::Pi - (*prev_PFrs_means)[int(mbx)] <<" "<< dPFr_local << std::endl;
+								std::cout << "ANGLEBends pi_PFrs " << ANGLEBends[zMatRow] <<" "<< SimTK::Pi - std::acos(X_PF.R()(0)(0)) << std::endl;
+								std::cout << "ANGLEBends PFrs_mean pi_PFrs_mean dPFr_local" <<" "<< ANGLEBends[zMatRow] <<" "<< (*prev_PFrs_means)[int(mbx)] <<" "<< SimTK::Pi - (*prev_PFrs_means)[int(mbx)] <<" "<< dPFr_local << std::endl;
 
 								if(numUs == 3){
 									if(localQIndex == 0){
--- src/World.cpp
+++ src/World.cpp
@@ -1931,7 +1931,7 @@ void World::calcSimbodyBAT(std::vector<std::vector<int>>& ZMatrix, std::vector<S
 		ZMatrix.resize(matter->getNumBodies() - 1, std::vector<int>(4, -1));
 	}
 
-	bool printTransforms = false;
+	bool printTransforms = true;
 
 	for (SimTK::MobilizedBodyIndex childMbx(1); childMbx < matter->getNumBodies(); ++childMbx){
 		childIx = int(childMbx);
